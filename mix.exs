bf11598a9c020310878870f9a8b00ef8fb449448
7_5P <76pollet@gmail.com>
2019-12-03 21:51:50 +0300

    add the uuid dependency to the mix file

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Tickets.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:cowboy, "~> 2.5"},
-      {:plug_cowboy, "~> 2.1"}
+      {:plug_cowboy, "~> 2.1"},
+      {:uuid, "~> 1.1" }
     ]
   end
 
